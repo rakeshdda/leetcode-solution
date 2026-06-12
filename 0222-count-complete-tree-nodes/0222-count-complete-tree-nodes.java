c8146c92d85f76ff6cc7a8eb3ee19e0217fee27d
Rakesh Das <dasrakesh46931@gmail.com>
2026-06-12 10:36:54 +0530

    Time: 0 ms (100%), Space: 49.4 MB (47.07%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -19,9 +19,8 @@ class Solution {
             return 0;
         }
 
-        // if(root.left == null || root.right == null){
-        //     return -1;
-        // }
+        
         return 1+ countNodes(root.left) + countNodes(root.right);
+        //the 1 is for the root itself
     }
 }
