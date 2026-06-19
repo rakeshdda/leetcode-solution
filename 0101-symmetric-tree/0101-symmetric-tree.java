26b8b0b88bdb1b261a2016f91552015fe04666be
Rakesh Das <dasrakesh46931@gmail.com>
2026-06-19 22:55:47 +0530

    Time: 0 ms (100%), Space: 43.5 MB (69.37%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,29 +15,53 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-       Queue<TreeNode> queue = new LinkedList<>();
-       queue.add(root.left);
-       queue.add(root.right);
 
-       while(!queue.isEmpty()){
-        TreeNode left = queue.poll();
-        TreeNode right = queue.poll();
+    //Bfs
+    //    Queue<TreeNode> queue = new LinkedList<>();
+    //    queue.add(root.left);
+    //    queue.add(root.right);
 
-        if(left == null && right ==null){
-            continue;
+    //    while(!queue.isEmpty()){
+    //     TreeNode left = queue.poll();
+    //     TreeNode right = queue.poll();
+
+    //     if(left == null && right ==null){
+    //         continue;
+    //     }
+    //     if(left == null || right == null ){
+    //         return false;
+    //     }
+    //     if(left.val != right.val){
+    //         return false;
+    //     }
+
+    //     queue.add(left.left);
+    //     queue.add(right.right);
+    //     queue.add(left.right);
+    //     queue.add(right.left);
+    //    } 
+    //    return true;
+
+
+    // recursive
+    if(root==null){
+        return true;
+    }
+
+    return isMirror(root.left, root.right);
+    }
+    public boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
         }
         if(left == null || right == null ){
             return false;
         }
+
         if(left.val != right.val){
             return false;
         }
 
-        queue.add(left.left);
-        queue.add(right.right);
-        queue.add(left.right);
-        queue.add(right.left);
-       } 
-       return true;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 }
