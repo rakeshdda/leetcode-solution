8554baff0f5aeb2426d2b091d2b61a332800ca06
Rakesh Das <dasrakesh46931@gmail.com>
2026-06-20 20:07:29 +0530

    Time: 0 ms (100%), Space: 44 MB (32.2%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,22 +15,50 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-		preOrder(root, queue );
-		
-		while(!queue.isEmpty()){
-			TreeNode node = queue.poll();
-			node.left = null;
-			node.right= queue.peek();
-		}        
-    }
-    public void preOrder(TreeNode node, Queue<TreeNode> queue){
-          if(node == null){
-        	return;
+
+        //normal preorder.  extra space O(n)
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     preOrder(root, queue);
+
+    //     while (!queue.isEmpty()) {
+    //         TreeNode node = queue.poll();
+    //         node.left = null;
+    //         node.right = queue.peek();
+    //     }
+    // }
+
+    // public void preOrder(TreeNode node, Queue<TreeNode> queue) {
+    //     if (node == null) {
+    //         return;
+    //     }
+
+    //     queue.offer(node);
+    //     preOrder(node.left, queue);
+    //     preOrder(node.right, queue);
+
+
+
+
+     //O(1) space 
+    TreeNode current = root;
+    while(current != null){
+        if(current.left != null){
+            TreeNode temp = current.left;
+            while(temp.right != null){
+                temp = temp.right;
+            }
+            temp.right = current.right;
+            current.right = current.left;
+            current.left = null;
         }
-        
-        queue.offer(node);
-        preOrder(node.left, queue);
-        preOrder(node.right, queue);
+        current = current.right;
+    }
+
+    // 1)take a movable pointer at the root as cuurent;
+    // 2) while current != null check if the the left subtree of current is not null , if not then find the rightmost node of the left subtree . let say x
+    // 3) then connect the right subtree of current as the right of the right most node in the left subtree (example as x.right) 
+    // 4) then make the left subtree of current as right, and make the left null, current.left = null
+    // 5) finally move your current to next right node to do the same
+
     }
 }
