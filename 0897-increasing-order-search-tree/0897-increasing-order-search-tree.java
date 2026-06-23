641ac4aacfcd99fe4f8d1272d0b873aa600daada
Rakesh Das <dasrakesh46931@gmail.com>
2026-06-23 21:49:17 +0530

    Time: 0 ms (100%), Space: 42.9 MB (63.07%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.java
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.java
@@ -14,25 +14,44 @@
  * }
  */
 class Solution {
-    public TreeNode increasingBST(TreeNode root) {
-         Queue<TreeNode> queue = new LinkedList<>();
-        inOrder(root, queue);
-        TreeNode newroot = queue.peek();
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            node.left = null;
-            node.right = queue.peek();
-        }
-        return newroot;
-    }
 
-    public void inOrder(TreeNode node, Queue<TreeNode> queue) {
+       TreeNode dummy = new TreeNode(-1);
+       TreeNode prev = dummy;
+    public TreeNode increasingBST(TreeNode root) {
+        //      Queue<TreeNode> queue = new LinkedList<>();
+        //     inOrder(root, queue);
+        //     TreeNode newroot = queue.peek();
+        //     while (!queue.isEmpty()) {
+        //         TreeNode node = queue.poll();
+        //         node.left = null;
+        //         node.right = queue.peek();
+        //     }
+        //     return newroot;
+        // }
+
+        // public void inOrder(TreeNode node, Queue<TreeNode> queue) {
+        //     if (node == null) {
+        //         return;
+        //     }
+
+        //     inOrder(node.left, queue);
+        //     queue.offer(node);
+        //     inOrder(node.right, queue);
+
+        inorder(root);
+        return dummy.right;
+    }
+    public void inorder(TreeNode node){
         if (node == null) {
             return;
         }
 
-        inOrder(node.left, queue);
-        queue.offer(node);
-        inOrder(node.right, queue);
+        inorder(node.left);
+
+        prev.right = node;
+        node.left = null;
+        prev = node;
+
+        inorder(node.right);
     }
 }
