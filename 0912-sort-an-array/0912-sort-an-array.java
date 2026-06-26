26b09fc16d3079b570937e269c223c204dd84f34
Rakesh Das <dasrakesh46931@gmail.com>
2026-06-26 21:44:54 +0530

    Time: 33 ms (25.58%), Space: 86.5 MB (25.24%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -2,36 +2,62 @@ class Solution {
     public int[] sortArray(int[] nums) {
         int n = nums.length;
 
-        for(int i=n/2 -1; i>=0; i--){
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(nums, n, i);
         }
 
-        for(int i= n-1; i>0; i--){
+        for (int i = n - 1; i > 0; i--) {
             swap(nums, i, 0);
 
-            heapify(nums,i, 0);
+            heapify(nums, i, 0);
         }
         return nums;
     }
-    public void heapify(int[] arr, int n, int i){
-        int largest =i;
-        int left = 2*i +1;
-        int right = 2*i +2;
+    // public void heapify(int[] arr, int n, int i){
+    //     int largest =i;
+    //     int left = 2*i +1;
+    //     int right = 2*i +2;
 
-        if(left <n && arr[left] > arr[largest]){
-            largest = left;
-        }
-        if(right < n && arr[right] > arr[largest]){
-            largest = right;
-        }
-        if(largest != i){
-            swap(arr, largest, i);
+    //     if(left <n && arr[left] > arr[largest]){
+    //         largest = left;
+    //     }
+    //     if(right < n && arr[right] > arr[largest]){
+    //         largest = right;
+    //     }
+    //     if(largest != i){
+    //         swap(arr, largest, i);
 
-            heapify(arr, n, largest);
+    //         heapify(arr, n, largest);
+    //     }
+    // }
+
+   void heapify(int[] arr, int n, int i) {
+
+        while (true) {
+
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest]) {
+                largest = left;
+            }
+
+            if (right < n && arr[right] > arr[largest]) {
+                largest = right;
+            }
+
+            if (largest == i) {
+                break; // Heap property restored
+            }
+
+            swap(arr, i, largest);
+
+            i = largest; // Continue heapifying from the child
         }
     }
 
-    public void swap(int [] arr, int first, int second){
+    public void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
