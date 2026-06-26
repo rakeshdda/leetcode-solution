362daf48655129ac723b39596f9685795cfc65b1
Rakesh Das <dasrakesh46931@gmail.com>
2026-06-26 21:39:12 +0530

    Time: 41 ms (19.28%), Space: 86.3 MB (41.37%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+
+        for(int i=n/2 -1; i>=0; i--){
+            heapify(nums, n, i);
+        }
+
+        for(int i= n-1; i>0; i--){
+            swap(nums, i, 0);
+
+            heapify(nums,i, 0);
+        }
+        return nums;
+    }
+    public void heapify(int[] arr, int n, int i){
+        int largest =i;
+        int left = 2*i +1;
+        int right = 2*i +2;
+
+        if(left <n && arr[left] > arr[largest]){
+            largest = left;
+        }
+        if(right < n && arr[right] > arr[largest]){
+            largest = right;
+        }
+        if(largest != i){
+            swap(arr, largest, i);
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public void swap(int [] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
