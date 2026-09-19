69737ad616b4648ce5f578e38228de3ddc6c782d
Rakesh Das <dasrakesh46931@gmail.com>
2026-09-19 14:55:13 +0530

    Time: 2 ms (87.58%), Space: 44.3 MB (24.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,68 +1,73 @@
 class Solution {
     class Pair{
-        int row ;
-        int col ;
+        int row;
+        int col;
+    
     Pair(int row, int col){
         this.row = row;
         this.col = col;
     }
     }
     public int orangesRotting(int[][] grid) {
-        int m= grid.length;
-        int n = grid[0].length;
+    int m= grid.length;
+    int n= grid[0].length;
+    
+    Queue<Pair> queue = new LinkedList<>();
 
-        int freshOrange =0;
+    int freshOrange =0;
 
-        Queue<Pair> queue = new LinkedList<>();
+    for(int i =0; i< m; i++){
+        for(int j=0; j< n; j++){
 
-        for(int i =0; i< m; i++){
-            for(int j =0; j< n; j++){
-                if(grid[i][j] == 2){
-                    queue.offer(new Pair(i,j));
-                }else if(grid[i][j] == 1){
-                    freshOrange++;
-                }
+            if(grid[i][j] == 2){
+                queue.offer(new Pair(i, j));
             }
+            else if(grid[i][j] == 1){
+                freshOrange++;
+            }
+
         }
-        if(freshOrange == 0){
-            return 0;
-        }
+    }
+    if(freshOrange ==0){
+        return 0;
+    }
+
+    int minute =0;
+
+    while(!queue.isEmpty()){
         
-        int [] dr = {-1,0,1,0};
-        int [] dc = {0,1,0,-1};
+        int qSize = queue.size();
 
-        int count =0;
+        for(int i=0; i< qSize; i++){
+            Pair current =queue.poll();
 
-        while(!queue.isEmpty()){
-
-            int qSize = queue.size();
-
-            while(qSize-- >0){
-            Pair current = queue.poll();
-
-            int row= current.row;
+            int row = current.row;
             int col = current.col;
 
-            for(int k =0; k< 4; k++){
-                int newRow = row + dr[k];
-                int newCol = col + dc[k];
+            int [] dr ={-1,0,1,0};
+            int [] dc ={0, 1,0, -1};
+
+            for(int j=0; j< 4; j++){
+                int newRow =row + dr[j];
+                int newCol = col + dc[j];
 
 
-                if(newRow >=0 && newRow< m && newCol >=0 && newCol < n){
-                    if(grid[newRow][newCol] == 1){
-                        grid[newRow][newCol] =2;
-                        queue.offer(new Pair(newRow, newCol));
-                        freshOrange--;
-                    }
+                if(newRow >=0 && newRow < m && newCol >=0 && newCol < n && grid[newRow][newCol] == 1){
+                    grid[newRow][newCol] = 2;
+
+                    queue.offer(new Pair(newRow, newCol));
+
+                    freshOrange--;
                 }
             }
-        } 
-        count++;
         }
-         if(freshOrange != 0){
-            return -1;
-         }
+        minute++;
+    }
 
-        return count-1;
+    if(freshOrange != 0){
+        return -1;
+    }
+
+    return minute -1;
     }
 }
